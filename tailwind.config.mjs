6c81e52016a39296e733a1b304ccd06eb0003005
Zairanww <zliu11@risd.edu>
2024-03-11 01:26:25 -0400

    project-draft

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		screens: {
+
+      'md': '600px',
+      // => @media (min-width: 1024px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1280px) { ... }
+    },
+		extend: {
+			fontFamily:{
+			'sans': [ "Work Sans", 'sans-serif'],
+			},
+			backgroundImage:{
+				'lgGrid':"url('./public/lg-bg.png')",
+			}
+		},
 	},
 	plugins: [],
 }
