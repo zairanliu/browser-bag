5baea5c70d7ac14d7bdf7e6f28b8c6881c06828d
Jason Gao <jasongao97@gmail.com>
2024-03-12 13:03:15 +0800

    fix path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import relativeLinks from "astro-relative-links";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [relativeLinks(), tailwind()],
 });
